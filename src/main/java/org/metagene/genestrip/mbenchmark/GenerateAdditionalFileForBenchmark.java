d7407e3603643e922b4820b9cc911013f7c3c66b
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-02-20 13:18:33 +0100

    .

--- src/main/java/org/metagene/genestrip/mbenchmark/GenerateAdditionalFileForBenchmark.java
+++ src/main/java/org/metagene/genestrip/mbenchmark/GenerateAdditionalFileForBenchmark.java
@@ -9,6 +9,8 @@ import org.metagene.genestrip.GSMaker;
 import org.metagene.genestrip.GSProject;
 import org.metagene.genestrip.genbank.AssemblySummaryReader;
 import org.metagene.genestrip.io.StreamProvider;
+import org.metagene.genestrip.make.ObjectGoal;
+import org.metagene.genestrip.tax.TaxTree;
 
 import java.io.*;
 import java.util.*;
@@ -18,6 +20,7 @@ public class GenerateAdditionalFileForBenchmark {
             .setDelimiter('\t').setRecordSeparator('\n').build();
 
     private static final String INPUT_RESOURCE = "nucleotide_database_filelist.txt";
+    private static final String SHORT_LIST_RESOURCE = "shortlist.txt";
 
     private final File baseDir;
 
@@ -25,12 +28,31 @@ public class GenerateAdditionalFileForBenchmark {
         this.baseDir = baseDir;
     }
 
-    public Set<String> generate(String dbName) throws IOException {
+    public Set<String> generate(String dbName, boolean withShortList) throws IOException {
         GSCommon config = new GSCommon(baseDir);
         GSProject project = new GSProject(config, dbName, null, null, null, null, null, false, null,
                 null, null, null, false);
         GSMaker maker = new GSMaker(project);
         maker.getGoal(GSGoalKey.ASSEMBLYDOWNLOAD).make();
+
+        ObjectGoal<TaxTree, GSProject> taxTreeGoal = (ObjectGoal<TaxTree, GSProject>) maker.getGoal(GSGoalKey.TAXTREE);
+        TaxTree tree = taxTreeGoal.get();
+        Set<TaxTree.TaxIdNode> nodes = new HashSet<>();
+        if (withShortList) {
+            try (BufferedReader lr = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(SHORT_LIST_RESOURCE)))) {
+                String line;
+                while ((line = lr.readLine()) != null) {
+                    String taxid = line.trim();
+                    TaxTree.TaxIdNode node = tree.getNodeByTaxId(taxid);
+                    if (node != null) {
+                        nodes.add(node);
+                    }
+                    else {
+                        throw new RuntimeException("Tax id " + taxid + " not found");
+                    }
+                }
+            }
+        }
 
         Set<String> files = new HashSet<String>();
         try (BufferedReader lr = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(INPUT_RESOURCE)))) {
@@ -41,7 +63,7 @@ public class GenerateAdditionalFileForBenchmark {
                 files.add(name);
             }
         }
-        ;
+
         Set<String> found = new HashSet<>();
         Set<String> taxids = new HashSet<>();
         try (PrintStream ps = new PrintStream(new File(project.getProjectDir(), "additional.txt"))) {
@@ -56,16 +78,19 @@ public class GenerateAdditionalFileForBenchmark {
                     String ftp = record.get(19);
                     AssemblySummaryReader.FTPEntryQuality quality = AssemblySummaryReader.FTPEntryQuality.fromString(complete, latest);
                     AssemblySummaryReader.FTPEntryWithQuality ewq = new AssemblySummaryReader.FTPEntryWithQuality(taxid, ftp, quality, null, false, speciesTaxid);
-                    if (files.contains(name)) {
-                        found.add(name);
-                        ps.print(taxid);
-                        ps.print(' ');
-                        ps.print(ewq.getFileName());
-                        ps.print(' ');
-                        ps.print(ewq.getFtpURL());
-                        ps.print('/');
-                        ps.println(ewq.getFileName());
-                        taxids.add(taxid);
+                    TaxTree.TaxIdNode node = tree.getNodeByTaxId(taxid);
+                    if (!withShortList || containsNodeOrSupernode(tree, nodes, node)) {
+                        if (files.contains(name)) {
+                            found.add(name);
+                            ps.print(taxid);
+                            ps.print(' ');
+                            ps.print(ewq.getFileName());
+                            ps.print(' ');
+                            ps.print(ewq.getFtpURL());
+                            ps.print('/');
+                            ps.println(ewq.getFileName());
+                            taxids.add(taxid);
+                        }
                     }
                 }
             }
@@ -79,8 +104,19 @@ public class GenerateAdditionalFileForBenchmark {
         return files;
     }
 
+    private boolean containsNodeOrSupernode(TaxTree tree, Set<TaxTree.TaxIdNode> nodes, TaxTree.TaxIdNode node) {
+        if (node != null) {
+            for (TaxTree.TaxIdNode n : nodes) {
+                if (tree.isAncestorOf(node, n)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
-        Set<String> missing = new GenerateAdditionalFileForBenchmark(new File("./data")).generate("mbenchmark");
+        Set<String> missing = new GenerateAdditionalFileForBenchmark(new File("./data")).generate("mbenchmark", false);
         System.out.println("Missing Accessions (" + missing.size() + " Values):");
         System.out.println(missing);
     }
