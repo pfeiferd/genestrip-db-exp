ddcca1c4b41eb02742e20f0dade99b9cc63f201e
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-03-29 19:19:00 +0100

    .

--- bin/make_ku_viraldb.sh
+++ bin/make_ku_viraldb.sh
@@ -7,7 +7,7 @@ cd $scriptdir/../ku/krakenuniq
 
 # Create a viral database
 mkdir ../viral_db
-./krakenuniq-download --db ../viral_db refseq/viral
+./krakenuniq-download --db ../viral_db refseq/viral/Any
 ./krakenuniq-download --db ../viral_db taxonomy
 
 export JELLYFISH_BIN=$scriptdir/../ku/krakenuniq/jellyfish-install/bin/jellyfish
