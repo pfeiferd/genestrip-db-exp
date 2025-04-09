ab763e03e95a5ecd0c7de9a32ae9270caad972f7
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-04-09 12:11:30 +0200

    .

--- src/main/java/org/metagene/genestrip/exp/ExperimentRunner.java
+++ src/main/java/org/metagene/genestrip/exp/ExperimentRunner.java
@@ -57,6 +57,6 @@ public class ExperimentRunner {
         runner.runMatchComparison("chronicb", "fastq/ticks.txt");
          */
         KrakenMatchComparator c = new KrakenMatchComparator(new File("./data"));
-        c.compareWithKUResults("viral", "viral_ku_comp_fasta.txt", "viral_ku_comp.txt");
+        c.compareWithKUResults("viral", "viral_ku_comp_fasta.txt", "viral_ku_comp.txt", new String[] { "fastq1" });
     }
 }
