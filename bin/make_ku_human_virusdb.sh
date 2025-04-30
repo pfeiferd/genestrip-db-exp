f05020c16efbc87c00e94d930b1955254adccc30
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-04-30 15:24:04 +0200

    .

--- bin/make_ku_human_virusdb.sh
+++ bin/make_ku_human_virusdb.sh
@@ -8,7 +8,7 @@ cd $scriptdir/../ku/krakenuniq
 # Create a human_virus database
 mkdir -p ../human_virus_db
 
-file_path=../../data/projects/human_virus/taxids.txt
+file_path=../../data/projects/human_virus/unfolded_taxids.txt
 
 line_number=1
 while read -r line; do
