d4bf46dfdf84eeabbaec5fef83180cfc2f44e523
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-05-04 10:44:57 +0200

    .

--- src/main/java/org/metagene/genestrip/exp/ExperimentRunner.java
+++ src/main/java/org/metagene/genestrip/exp/ExperimentRunner.java
@@ -60,10 +60,10 @@ public class ExperimentRunner {
         c.reportKMerComparisons("viral", false, "viral_db");
          */
 
-        // KrakenMatchComparator c2 = new KrakenMatchComparator(new File("./data"));
+        KrakenMatchComparator c2 = new KrakenMatchComparator(new File("./data"));
         // c2.accuracyCheckForSimulatedViralReads("viral", "viral_ku_comp.txt");
         // c2.compareWithKUResults("viral", "viral_ku_comp_fasta.txt", "viral_ku_comp.txt", new String[] { "fastq1" });
-        // c2.compareWithKUResults("viral", null, "saliva.txt", new String[] { "saliva" });
+        c2.compareWithKUResults("viral", null, "saliva.txt", new String[] { "saliva" });
         // c2.compareWithKUResults("human_virus", null, "saliva.txt", new String[] { "saliva" });
 
         SimpleMatchComparator c1 = new SimpleMatchComparator(new File("./data"));
