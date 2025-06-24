2af5fa774a2214a869536dd009e4464fd8542099
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-06-24 11:29:49 +0200

    x

--- bin/install_sra_sdk_linux64.sh
+++ bin/install_sra_sdk_linux64.sh
@@ -10,6 +10,6 @@ cd sra-tools
 
 wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.2.1/sratoolkit.3.2.1-alma_linux64.tar.gz
 gunzip sratoolkit.3.2.1-alma_linux64.tar.gz
-tar -xf sratoolkit.3.2.1-alma_linux64.tar.gz
+tar -xf sratoolkit.3.2.1-alma_linux64.tar
 mv sratoolkit.3.2.1-alma_linux64 sratoolkit
 rm sratoolkit.3.2.1-alma_linux64.tar
