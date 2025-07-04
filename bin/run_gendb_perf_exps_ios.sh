6ce466815678c7b9975ed4421753f4c9b1d85bfc
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-04 11:04:48 +0200

    x

--- bin/run_gendb_perf_exps_ios.sh
+++ bin/run_gendb_perf_exps_ios.sh
@@ -5,17 +5,11 @@ scriptdir=$(dirname "$0")
 
 cd $scriptdir/..
 
-if [ -z "$1" ]
-  then
-	goal=db
-  else
-  	goal=$1
-fi
-
-/usr/bin/time -l mvn exec:exec@db -Dname=human_virus -Dgoal=$goal  >& data/db_gen_hv.log
-mvn exec:exec@db -Dname=human_virus -Dgoal=dbinfo
-#/usr/bin/time -l mvn exec:exec@db -Dname=virus -Dgoal=$goal  >& db_gen_cv.log
-#/usr/bin/time -l mvn exec:exec@db -Dname=tick-borne -Dgoal=$goal  >& db_gen_tb.log
-#/usr/bin/time -l mvn exec:exec@db -Dname=vineyard -Dgoal=$goal  >& db_gen_vin.log
-#/usr/bin/time -l mvn exec:exec@db -Dname=protozoa -Dgoal=$goal  >& db_gen_pro.log
-#/usr/bin/time -l mvn exec:exec@db -Dname=parasites -Dgoal=$goal  >& db_gen_par.log
+for db in human_virus; # viral tick-borne vineyard protozoa parasites;
+  do
+    mvn exec:exec@db -Dname=$db -Dgoal=clear
+    mvn exec:exec@db -Dname=$db -Dgoal=refseqfna
+    mvn exec:exec@db -Dname=$db -Dgoal=fastasgenbankdl
+    /usr/bin/time -l mvn exec:exec@db -Dname=$db -Dgoal=db  >& data/db_gen_$(db).log
+    mvn exec:exec@db -Dname=$db -Dgoal=dbinfo
+  done
