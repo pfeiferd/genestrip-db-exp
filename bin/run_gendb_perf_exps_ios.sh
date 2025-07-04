5c3461adb77ea92dce7f0af25a247cda559bccb8
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-04 11:39:52 +0200

    x

--- bin/run_gendb_perf_exps_ios.sh
+++ bin/run_gendb_perf_exps_ios.sh
@@ -5,11 +5,11 @@ scriptdir=$(dirname "$0")
 
 cd $scriptdir/..
 
-for db in human_virus; # viral tick-borne vineyard protozoa parasites;
+for db in human_virus viral tick-borne; #vineyard protozoa parasites;
   do
     mvn exec:exec@db -Dname=$db -Dgoal=clear
     mvn exec:exec@db -Dname=$db -Dgoal=refseqfna
     mvn exec:exec@db -Dname=$db -Dgoal=fastasgenbankdl
-    /usr/bin/time -l mvn exec:exec@db -Dname=$db -Dgoal=db  >& data/db_gen_$(db).log
+    /usr/bin/time -l mvn exec:exec@db -Dname=$db -Dgoal=db  >& data/db_gen_${db}.log
     mvn exec:exec@db -Dname=$db -Dgoal=dbinfo
   done
