b2227a769c716d4a0173e37b96bc0cc841689be3
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-04 17:57:20 +0200

    x

--- bin/download_hs_fastqs.sh
+++ bin/download_hs_fastqs.sh
@@ -9,18 +9,13 @@ bin=./sra-tools/sratoolkit/bin
 file_path=./data/fastq
 
 # ERX1462737
-$bin/prefetch ERR1395610 --max-size 200g -O $file_path
 # ERX1462740
-$bin/prefetch ERR1395613 --max-size 200g -O $file_path
 # SRX2830683
-$bin/prefetch SRR5571991 --max-size 200g -O $file_path
 # SRX2830684
-$bin/prefetch SRR5571990 --max-size 200g -O $file_path
 # SRX2830689
-$bin/prefetch SRR5571985 --max-size 200g -O $file_path
-
-$bin/fasterq-dump $file_path/ERR1395610 -O $file_path
-$bin/fasterq-dump $file_path/ERR1395613 -O $file_path
-$bin/fasterq-dump $file_path/SRR5571991 -O $file_path
-$bin/fasterq-dump $file_path/SRR5571990 -O $file_path
-$bin/fasterq-dump $file_path/SRR5571985 -O $file_path
+for id in ERR1395610 ERR1395613 SRR5571991 SRR5571990 SRR5571985;
+  do
+      # $bin/prefetch $id --max-size 200g -O $file_path
+      $bin/fasterq-dump $file_path/$id -O $file_path
+      gzip $file_path/$id_?.fastq
+  done
