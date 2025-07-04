dd884c7f7748023207db3ec6e934138569d7b68b
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-04 20:50:39 +0200

    x

--- bin/download_hs_fastqs.sh
+++ bin/download_hs_fastqs.sh
@@ -13,9 +13,10 @@ file_path=./data/fastq
 # SRX2830683
 # SRX2830684
 # SRX2830689
-for id in ERR1395610 ERR1395613 SRR5571991 SRR5571990 SRR5571985;
+for id in SRR5571991 SRR5571990 SRR5571985; # ERR1395610 ERR1395613
   do
-      # $bin/prefetch $id --max-size 200g -O $file_path
-      #$bin/fasterq-dump $file_path/$id -O $file_path
+      $bin/prefetch $id --max-size 200g -O $file_path
+      $bin/fasterq-dump $file_path/$id -O $file_path
       gzip $file_path/${id}_?.fastq
+      rm -rf $file_path/${id}
   done
