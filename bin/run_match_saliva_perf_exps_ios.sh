b32d9593b9aea29adad9c62410557fcb8ef70509
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-08 14:08:09 +0200

    x

---
+++ bin/run_match_saliva_perf_exps_ios.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+scriptdir=$(dirname "$0")
+
+cd $scriptdir/..
+
+for t in ERR1395613 ERR1395610 SRR5571991 SRR5571990 SRR5571985;
+  do
+    rm -f ./data/projects/viral/csv/viral_match_${t}.csv
+    /usr/bin/time -l mvn exec:exec@singlematch -Dname=viral -Dgoal=match -Dfqfile=${t}.fastq.gz  >& data/match_viral_${t}.log
+  done
