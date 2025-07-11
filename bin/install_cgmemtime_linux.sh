7935ae664bb15f7ee778bfd476cfe836c1d1db14
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-11 21:08:30 +0200

    x

--- bin/install_cgmemtime_linux.sh
+++ bin/install_cgmemtime_linux.sh
@@ -5,9 +5,6 @@ scriptdir=$(dirname "$0")
 
 cd $scriptdir/..
 
-mkdir -p sra-tools
-cd sra-tools
-
 wget https://github.com/gsauthof/cgmemtime/archive/refs/heads/master.zip
 unzip master.zip
 mv  cgmemtime-master  cgmemtime
