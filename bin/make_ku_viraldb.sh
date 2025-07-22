4c1d54fc3a7a319cc12fd5e677b8b177fb31fa13
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-07-22 15:05:00 +0200

    x

--- bin/make_ku_viraldb.sh
+++ bin/make_ku_viraldb.sh
@@ -10,11 +10,11 @@ mkdir -p ../viral_db
 ./krakenuniq-download --db ../viral_db refseq/viral/Any
 ./krakenuniq-download --db ../viral_db taxonomy
 # Remove this single genome as one of its k-mers messes up the validation:
-mkdir ../viral_db/library/viral/moved
-mv ../viral_db/library/viral/Chromosome/Tenuivirus_zeae_na-tax3052767* ../viral_db/library/viral/moved
-sed '/Tenuivirus_zeae_na-tax3052767/d' ../viral_db/library-files.txt > ../viral_db/library-files.txt.new
-mv ../viral_db/library-files.txt ../viral_db/library-files.txt.org
-mv ../viral_db/library-files.txt.new ../viral_db/library-files.txt
+#mkdir ../viral_db/library/viral/moved
+#mv ../viral_db/library/viral/Chromosome/Tenuivirus_zeae_na-tax3052767* ../viral_db/library/viral/moved
+#sed '/Tenuivirus_zeae_na-tax3052767/d' ../viral_db/library-files.txt > ../viral_db/library-files.txt.new
+#mv ../viral_db/library-files.txt ../viral_db/library-files.txt.org
+#mv ../viral_db/library-files.txt.new ../viral_db/library-files.txt
 
 export JELLYFISH_BIN=$(pwd)/jellyfish-install/bin/jellyfish
 
