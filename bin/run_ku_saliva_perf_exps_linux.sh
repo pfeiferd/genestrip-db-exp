630554613d7a33bc8ede164c899e2f3d2d07493d
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-08-08 14:17:05 +0200

    x

--- bin/run_ku_saliva_perf_exps_linux.sh
+++ bin/run_ku_saliva_perf_exps_linux.sh
@@ -10,5 +10,5 @@ res_path=./results
 
 for t in ERR1395613 ERR1395610 SRR5571991 SRR5571990 SRR5571985;
   do
-    ./cgmemtime/cgmemtime ./ku/krakenuniq/krakenuniq --report-file ${res_path}/reports/ku_${t}.tsv --output off --threads 10 -db ./ku/viral_db ${file_path}/${t}_1.fastq.gz  >& ${res_path}/logs/match_ku_viral_${t}.log
+    ./cgmemtime/cgmemtime ./ku/krakenuniq/krakenuniq --report-file ${res_path}/reports/ku_${t}.tsv --output off --threads 10 -db ./ku/viral_db ${file_path}/${t}_1.fastq.gz  > ${res_path}/logs/match_ku_viral_${t}.log
   done
