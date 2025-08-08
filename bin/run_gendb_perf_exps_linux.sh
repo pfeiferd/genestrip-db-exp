20b84a272be9599796dc19bf2b4e4f0de83ae9c2
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-08-08 13:01:18 +0200

    x

--- bin/run_gendb_perf_exps_linux.sh
+++ bin/run_gendb_perf_exps_linux.sh
@@ -10,8 +10,8 @@ res_path=./results
 for db in vineyard parasites human_virus viral tick-borne protozoa;
   do
     mvn exec:exec@db -Dname=$db -Dgoal=clear
-    #mvn exec:exec@db -Dname=$db -Dgoal=refseqfna
-    #mvn exec:exec@db -Dname=$db -Dgoal=fastasgenbankdl
+    mvn exec:exec@db -Dname=$db -Dgoal=refseqfna
+    mvn exec:exec@db -Dname=$db -Dgoal=fastasgenbankdl
     ./cgmemtime/cgmemtime mvn exec:exec@db -Dname=$db -Dgoal=db  >& ${res_path}/logs/db_gen_${db}.log
     mvn exec:exec@db -Dname=$db -Dgoal=dbinfo
   done
