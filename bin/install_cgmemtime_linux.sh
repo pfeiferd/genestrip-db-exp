6db41bc5929f0532f764adfa5f7db8369130ea9b
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-08-08 13:54:09 +0200

    x

--- bin/install_cgmemtime_linux.sh
+++ bin/install_cgmemtime_linux.sh
@@ -9,6 +9,8 @@ wget https://github.com/gsauthof/cgmemtime/archive/refs/heads/master.zip
 unzip master.zip
 mv  cgmemtime-master  cgmemtime
 cd  cgmemtime
+# Let' print to stdout:
+sed -i 's/print_result(stderr, &args, &res);/print_result(stdout, &args, &res);/g' cgmemtime.c
 make
 
 cd $scriptdir/..
