34a01c83981c26d472b7dfa7c7343fccd17d0c73
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-06 18:20:47 +0100

    .

--- bin/make_ganon_viraldb.sh
+++ bin/make_ganon_viraldb.sh
@@ -11,7 +11,7 @@ cd $scriptdir/../ganon
 mkdir -p viral_db
 
 # Requires FTP to work.
-ganon build --db-prefix viral_db --source refseq --organism-group viral --complete-genomes --threads 24
+ganon build --db-prefix viral_db --source refseq --organism-group viral --threads 24
 
 # Run ganon on simulated virus fastq from paper:
 ganon classify --db-prefix viral_db -s ../data/projects/viral/fastq/viral_fasta2fastq_fasta1.fastq.gz --output-all --output-all -o viralfastq1 --threads 32
