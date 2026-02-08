7fd8771544900857a7e68743ee60c3e8dab3dd22
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-08 12:20:54 +0100

    .

--- bin/make_iss_viral_fastq.sh
+++ bin/make_iss_viral_fastq.sh
@@ -13,5 +13,5 @@ mkdir -p iss/viral
 cp data/common/refseq/viral.1.1.genomic.fna.gz iss/viral
 gunzip iss/viral/viral.1.1.genomic.fna.gz
 
-iss generate --genomes ../iss/viral/viral.1.1.genomic.fna  --model miseq --output data/projects/viral/fastq/iss_miseq_viral_reads
-iss generate --genomes ../iss/viral/viral.1.1.genomic.fna  --model hiseq --output data/projects/viral/fastq/iss_hiseq_viral_reads
+iss generate --genomes iss/viral/viral.1.1.genomic.fna  --model miseq --output data/projects/viral/fastq/iss_miseq_viral_reads
+iss generate --genomes iss/viral/viral.1.1.genomic.fna  --model hiseq --output data/projects/viral/fastq/iss_hiseq_viral_reads
