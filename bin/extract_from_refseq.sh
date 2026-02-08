bd1a3f5fef229bb9ad7e3f7df18929ad2b15047f
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-08 09:19:10 +0100

    .

--- bin/extract_from_refseq.sh
+++ bin/extract_from_refseq.sh
@@ -5,5 +5,6 @@ scriptdir=$(dirname "$0")
 
 cd $scriptdir/..
 
+mvn exec:exec@match -Dname=tick-borne -Dgoal=extractrefseqcsv
 mvn exec:exec@match -Dname=human_virus -Dgoal=extractrefseqcsv
 mvn exec:exec@match -Dname=viral -Dgoal=extractrefseqcsv
