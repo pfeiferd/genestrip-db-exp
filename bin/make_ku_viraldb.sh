93179c84a3e9fed86c7a47c4ceca71abe910590c
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-08 10:55:38 +0100

    .

--- bin/make_ku_viraldb.sh
+++ bin/make_ku_viraldb.sh
@@ -16,7 +16,9 @@ mkdir -p ../viral_db/taxonomy
 cp ../../data/common/nodes.dmp ../viral_db/taxonomy
 cp ../../data/common/names.dmp ../viral_db/taxonomy
 
-cp ../../data/projects/viral/fasta/*.fasta.gz ../viral_db/library
+cp ../../data/projects/viral/fasta/*.fa.gz ../viral_db/library
+# KU only accepts unzipped files ending with fa, fna and so on
+gunzip ../../data/projects/viral/fasta/*.fa.gz
 cp ../../data/projects/viral/csv/viral_ku.map ../viral_db/library
 
 export JELLYFISH_BIN=$(pwd)/jellyfish-install/bin/jellyfish
