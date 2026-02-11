1bc0ce325d2da0ced977d507e4f94dfe070a5723
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-11 11:37:15 +0100

    .

--- bin/buildK2.sh
+++ bin/buildK2.sh
@@ -10,12 +10,12 @@ cd $basedir/k2/kraken2
 
 for file in $basedir/ku/viral_db/library/*.fa
 do
-    kraken2-build --add-to-library $file --db ../viral_db
+    ./kraken2-build --add-to-library $file --db ../viral_db
 done
-kraken2-build --build --db ../viral_db
+./kraken2-build --build --db ../viral_db
 
 for file in $basedir/ku/human_virus_db/library/*.fa
 do
-    kraken2-build --add-to-library $file --db ../human_virus_db
+    ./kraken2-build --add-to-library $file --db ../human_virus_db
 done
-kraken2-build --build --db ../human_virus_db
+./kraken2-build --build --db ../human_virus_db
