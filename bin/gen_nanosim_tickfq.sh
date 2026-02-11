6af6959c77ab71cbaba41069c6dfbd22f3336627
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-11 19:20:32 +0100

    .

--- bin/gen_nanosim_tickfq.sh
+++ bin/gen_nanosim_tickfq.sh
@@ -10,7 +10,7 @@ mkdir -p nanosim/viral
 mkdir -p nanosim/tick-borne
 
 # Does not work -c nanosim/viral/training, -c does not work at all
-read_analysis.py metagenome -gl data/projects/viral/csv/viral_nanosim.tsv -i data/fastq/tick1.fastq.gz  -t 24
-read_analysis.py metagenome -gl data/projects/tick-borne/csv/tick-borne_nanosim.tsv -i data/fastq/tick1.fastq.gz  -t 24
+read_analysis.py metagenome --fastq -gl data/projects/viral/csv/viral_nanosim.tsv -i data/fastq/tick1.fastq.gz  -t 24
+read_analysis.py metagenome --fastq -gl data/projects/tick-borne/csv/tick-borne_nanosim.tsv -i data/fastq/tick1.fastq.gz  -t 24
 
-# simulator.py metagenome -gl data/projects/viral/csv/viral_nanosim.tsv -t 24 -a abundance.tsv
+# simulator.py metagenome --fastq -gl data/projects/viral/csv/viral_nanosim.tsv -t 24 -a abundance.tsv
