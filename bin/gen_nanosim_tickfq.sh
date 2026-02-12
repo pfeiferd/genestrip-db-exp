c34d8b0ba96fbb131d77ccd898ca77519aabf72c
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-12 16:08:52 +0100

    .

--- bin/gen_nanosim_tickfq.sh
+++ bin/gen_nanosim_tickfq.sh
@@ -16,8 +16,7 @@ do
   ${nanosimdir}/read_analysis.py metagenome -q --fastq -gl data/projects/viral/csv/viral_nanosim.tsv -i data/fastq/${file}.fastq.gz  -t 24
   #${nanosimdir}/read_analysis.py metagenome --fastq -gl data/projects/tick-borne/csv/tick-borne_nanosim.tsv -i data/fastq/tick1.fastq.gz  -t 24
 
-  reads=10000
-  #$(zcat data/fastq/${file}.fastq.gz | wc -l | awk '{print $1/4}')
+  reads=$(zcat data/fastq/${file}.fastq.gz | wc -l | awk '{print $1/4}')
   sed -i "s/Abundance/$reads/g" training_quantification.tsv
   ${nanosimdir}/simulator.py metagenome --fastq -gl data/projects/viral/csv/viral_nanosim.tsv -t 24 -a training_quantification.tsv
   mv simulated_sample0_unaligned_reads.fastq data/fastq/${file}_sim.fastq
