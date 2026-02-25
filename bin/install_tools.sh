b525d5a7c057aac698f850da3df085c17e2f276e
professorgraf <90040478+professorgraf@users.noreply.github.com>
2026-02-25 20:13:16 +0100

    Fix compile error by removing gzstream/version
    Remove gzstream/version file to fix compile error and add instructions for uid_mapping.hpp fix.

--- bin/install_tools.sh
+++ bin/install_tools.sh
@@ -56,8 +56,10 @@ mkdir -p ku
 cd ku
 
 # We simply use the latest GitHub version... (currently v1.0.4)
+rm -RF krakenuniq
 git clone https://github.com/fbreitwieser/krakenuniq.git
-
+# if error uint32_t appears, create a fixed version of uid_mapping.hpp with #include <cstdint> and report-cols.hpp in $basedir/bin/fixes/ku/ folder. 
+# cp $basedir/bin/fixes/ku/*.hpp krakenuniq/src
 cd krakenuniq
 
 # This file must be deleted, it leads to a compile error as it is not C++
