830a4b121346ff01037f3e1936761aca0cf3c305
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-03-01 21:56:03 +0100

    .

--- bin/gen_nanosim_tickfq.sh
+++ bin/gen_nanosim_tickfq.sh
@@ -18,10 +18,10 @@ nanosimdir=${basedir}/../NanoSim/src
 for file in tick1 tick2 tick3 tick4 tick5 tick6 tick7 tick8
 do
   #${nanosimdir}/read_analysis.py metagenome -q --fastq -gl data/projects/viral/csv/viral_nanosim.tsv -i data/fastq/${file}.fastq.gz  -t 24
-  ${nanosimdir}/read_analysis.py metagenome -q --fastq -gl data/projects/tick-borne/csv/tick-borne_nanosim.tsv -i data/fastq/${file}.fastq.gz  -t 24
+  #${nanosimdir}/read_analysis.py metagenome -q --fastq -gl data/projects/tick-borne/csv/tick-borne_nanosim.tsv -i data/fastq/${file}.fastq.gz  -t 24
 
-  reads=$(zcat data/fastq/${file}.fastq.gz | wc -l | awk '{print $1/4}')
-  sed -i "s/Abundance/$reads/g" training_quantification.tsv
+  #reads=$(zcat data/fastq/${file}.fastq.gz | wc -l | awk '{print $1/4}')
+  #sed -i "s/Abundance/$reads/g" training_quantification.tsv
   #${nanosimdir}/simulator.py metagenome --fastq -gl data/projects/viral/csv/viral_nanosim.tsv -t 24 -a training_quantification.tsv
   ${nanosimdir}/simulator.py metagenome --perfect --fastq -gl data/projects/tick-borne/csv/tick-borne_nanosim.tsv -t 24 -a training_quantification.tsv
   mv simulated_sample0_unaligned_reads.fastq data/fastq/${file}_sim.fastq
