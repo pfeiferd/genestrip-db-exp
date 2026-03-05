b7b2a7cf9dc055a78c9dfe3117e906d1fe9a8463
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-03-05 14:01:06 +0100

    .

--- src/main/java/org/metagene/genestrip/exp/AccuracyComparator.java
+++ src/main/java/org/metagene/genestrip/exp/AccuracyComparator.java
@@ -114,10 +114,12 @@ public class AccuracyComparator extends GenestripComparator {
 
         Map<String, Integer> totals = new HashMap<>();
         Map<String, int[]> resGenestrip = accuracyForSimulatedReadsGenestrip(db, mapFile, checkTree, true, false);
+        /*
         Map<String, int[]> resGenestripHighSens = accuracyForSimulatedReadsGenestrip(db, mapFile, checkTree, true, true);
         Map<String, int[]> resKU = accuracyForSimulatedReadsKU(db, mapFile, checkTree, false, 0, true);
         Map<String, int[]> resK2 = accuracyForSimulatedReadsKU(db, mapFile, checkTree, true, 0, true);
         Map<String, int[]> resK2HighConf = accuracyForSimulatedReadsKU(db, mapFile, checkTree, true, 0.4, true);
+         */
         Map<String, int[]> resGanon = new LinkedHashMap<>();
 
         try (PrintStream ps = new PrintStream(new FileOutputStream(new File(resultsDir, db + (checkDB == null ? "" : "_" + checkDB) + "_accuracy.csv")))) {
@@ -126,11 +128,13 @@ public class AccuracyComparator extends GenestripComparator {
                 int[] genestripCounts = resGenestrip.get(fastqKey);
                 int total = genestripCounts[5]; // No correct results without ground truth available.
                 totals.put(fastqKey, total);
+                /*
                 printCounts(ps, fastqKey, Sys.KRAKEN_UNIQ, resKU.get(fastqKey), total);
                 printCounts(ps, fastqKey, Sys.KRAKEN2, resK2.get(fastqKey), total);
                 printCounts(ps, fastqKey, Sys.KRAKEN2_HIGH_CONF, resK2HighConf.get(fastqKey), total);
+                 */
 
-                int[] counts = accuracyForSimulatedReadsGanon("standard", "ganon/" + db + "_" + fastqKey + ".all", checkTree, true);
+                int[] counts = accuracyForSimulatedReadsGanon(db, "ganon/" + db + "_" + fastqKey + ".all", checkTree, true);
                 printCounts(ps, fastqKey, Sys.GANON, counts, total);
                 resGanon.put(fastqKey, counts);
 
@@ -140,17 +144,17 @@ public class AccuracyComparator extends GenestripComparator {
                  */
 
                 printCounts(ps, fastqKey, Sys.GENESTRIP, genestripCounts, total);
-                printCounts(ps, fastqKey, Sys.GENESTRIP_HIGH_SENS, resGenestripHighSens.get(fastqKey), total);
+                //printCounts(ps, fastqKey, Sys.GENESTRIP_HIGH_SENS, resGenestripHighSens.get(fastqKey), total);
             }
         }
         try (PrintStream ps = new PrintStream(new FileOutputStream(new File(resultsDir, db + (checkDB == null ? "" : "_" + checkDB) + "_ma_accuracy.csv")))) {
             ps.println("system; precision genus; recall genus; f1 genus; precision species; recall species; f1 species;");
-            printMACounts(ps, Sys.KRAKEN_UNIQ, resKU, totals);
-            printMACounts(ps, Sys.KRAKEN2, resK2, totals);
-            printMACounts(ps, Sys.KRAKEN2_HIGH_CONF, resK2HighConf, totals);
+//            printMACounts(ps, Sys.KRAKEN_UNIQ, resKU, totals);
+//            printMACounts(ps, Sys.KRAKEN2, resK2, totals);
+//            printMACounts(ps, Sys.KRAKEN2_HIGH_CONF, resK2HighConf, totals);
             printMACounts(ps, Sys.GANON, resGanon, totals);
             printMACounts(ps, Sys.GENESTRIP, resGenestrip, totals);
-            printMACounts(ps, Sys.GENESTRIP_HIGH_SENS, resGenestripHighSens, totals);
+//            printMACounts(ps, Sys.GENESTRIP_HIGH_SENS, resGenestripHighSens, totals);
         }
     }
 
