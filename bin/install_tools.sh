7c71beabda0dcbf9cedf612459b0d736feb37146
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-03-05 15:45:16 +0100

    .

--- bin/install_tools.sh
+++ bin/install_tools.sh
@@ -68,6 +68,7 @@ cd krakenuniq
 rm src/gzstream/version
 # Fix missing include:
 sed -i '1i\#include <stdint.h>' src/uid_mapping.hpp
+sed -i '13s/class//' src/report-cols.hpp
 # Now we can build...
 ./install_krakenuniq.sh -j .
 
