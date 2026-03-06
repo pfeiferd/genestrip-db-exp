50f3f529e089f76cfc67b456dda46e6c37bd8b20
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-03-06 10:59:17 +0100

    .

--- bin/install_nanosim_ganon.sh
+++ bin/install_nanosim_ganon.sh
@@ -1,13 +1,3 @@
-# Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# Rest of Homebrew
-echo >>  ~/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >>  ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-# Miniconda - works on Mac - but on Linux?
-brew install miniconda
-
-
 ## THIS WORKED for me on LINUX
 
 # Conda
@@ -23,6 +13,7 @@ cd  ~
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
+
 # Nanosim
 git clone https://github.com/bcgsc/NanoSim.git
 conda create --name nanosim python=3.7
@@ -43,12 +34,11 @@ conda activate nanosim
 
 # Ganon:
 # Doesn't seem to work on a MAC (I can't compile it, I can't install it via conda).
-# Therefor just on Linux:
+# Therefore just on Linux:
 conda install -c bioconda -c conda-forge ganon
 
 # Requires FTP to work.
 ganon build --db-prefix viral_cg_rs --source refseq --organism-group viral --complete-genomes --threads 24
-
 
 # Run ganon on simulated virus fastq from paper:
 ganon classify --db-prefix viral_cg_rs data/projects/viral/fastq/viral_fasta2fastq_fasta1.fastq.gz --output-all results --threads 32
